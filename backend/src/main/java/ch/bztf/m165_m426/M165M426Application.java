0f406ccc76334a75dbdd813cd5d8e614f72101c9
Esteban Venturello <esteban.venturello@brueggli.ch>
2026-03-11 21:08:50 +0100

    feat: create backend template with Spring Initializr

---
+++ backend/src/main/java/ch/bztf/m165_m426/M165M426Application.java
@@ -0,0 +1,13 @@
+package ch.bztf.m165_m426;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M165M426Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M165M426Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/bztf/m165_m426/M165M426ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bztf.m165_m426;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M165M426ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
