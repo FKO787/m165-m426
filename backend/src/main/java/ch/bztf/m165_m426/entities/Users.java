e1d15aedf489c97cf3324c079ecbf20dde73c111
Esteban Venturello <esteban.venturello@brueggli.ch>
2026-03-12 15:46:01 +0100

    feat: create entity and repository with spring JPA

---
+++ backend/src/main/java/ch/bztf/m165_m426/entities/Users.java
@@ -0,0 +1,48 @@
+package ch.bztf.m165_m426.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    protected Users() {
+        // Hibernate requires Entities to have an empty constructor. Its protected so the IDE stops whining.
+    }
+
+    public Users(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
