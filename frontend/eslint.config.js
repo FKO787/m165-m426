e61d688ebb8a910f42ae259addc9c52f68384e9b
ShadowOfThePit <shadowof.thepit@gmail.com>
2026-03-28 15:00:05 +0100

    feat: replace prettier with stylistic, add formatting settings

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,48 +1,66 @@
 // @ts-check
-import JSeslint from '@eslint/js';
-import angular from 'angular-eslint';
-import { defineConfig } from 'eslint/config';
-import TSeslint from 'typescript-eslint';
+import JSeslint from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+import angular from "angular-eslint";
+import { defineConfig } from "eslint/config";
+import TSeslint from "typescript-eslint";
 
 export default defineConfig(
   {
-    files: ['**/*.ts'],
+    files: ["**/*.ts"],
 
     extends: [
       JSeslint.configs.recommended,
       TSeslint.configs.recommended,
       TSeslint.configs.stylistic,
       angular.configs.tsRecommended,
+      stylistic.configs.customize({ severity: "warn" }),
     ],
+
+    plugins: { "@stylistic": stylistic },
 
     processor: angular.processInlineTemplates,
 
     rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: '',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: '',
-          style: 'kebab-case',
-        },
-      ],
-    },
-  },
 
+      // Angular Rules
+
+      "@angular-eslint/directive-selector": [
+        "error", { type: "attribute", prefix: "", style: "camelCase" },
+      ],
+
+      "@angular-eslint/component-selector": [
+        "error", { type: "element", prefix: "", style: "kebab-case" },
+      ],
+
+      // Allow unused variables to be used by prepending an underscore "_"
+      "@typescript-eslint/no-unused-vars": ["error", {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      }],
+
+      // Stylistic Rules - https://eslint.style/rules/
+
+      "@stylistic/max-len": ["warn", {
+        code: 200,
+        ignoreUrls: true, ignoreStrings: true, ignoreTemplateLiterals: true,
+      }],
+      "@stylistic/no-tabs": "warn",
+      "@stylistic/semi": ["warn", "always"],
+      "@stylistic/comma-dangle": ["warn", "only-multiline"],
+      "@stylistic/quotes": ["warn", "double", { avoidEscape: true }],
+
+      "@stylistic/array-element-newline": ["warn", "consistent"],
+      "@stylistic/array-bracket-newline": ["warn", "consistent"],
+    }
+  },
   {
-    files: ['**/*.html'],
+    files: ["**/*.html"],
     extends: [
       angular.configs.templateRecommended,
-      angular.configs.templateAccessibility
+      angular.configs.templateAccessibility,
     ],
-    rules: {},
   },
 );
