265a9674ba8a9ff4041a2fdd46448e61e0fdaf35
ShadowOfThePit <shadowof.thepit@gmail.com>
2026-04-20 10:33:08 +0200

    refactor: move SecurityConfig into security directory

--- backend/src/main/java/ch/bztf/m165_m426/security/SecurityConfig.java
+++ backend/src/main/java/ch/bztf/m165_m426/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package ch.bztf.m165_m426.config;
+package ch.bztf.m165_m426.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +10,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import ch.bztf.m165_m426.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
