9f7f8c1d6d69ab736cd9007733c4fdbd68cea4a9
ShadowOfThePit <shadowof.thepit@gmail.com>
2026-04-22 08:17:06 +0200

    feat: change from prettier to stylistic

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,39 +1,64 @@
 // @ts-check
-import JSeslint from '@eslint/js';
-import angular from 'angular-eslint';
 import { defineConfig } from 'eslint/config';
-import TSeslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+import ESLintJS from '@eslint/js';
+import ESLintTS from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
-export default defineConfig(
+// Allow unused variables to be used by prepending an underscore '_'
+const unusedVarIgnorePattern = {
+  argsIgnorePattern: '^_',
+  varsIgnorePattern: '^_',
+  caughtErrorsIgnorePattern: '^_',
+  destructuredArrayIgnorePattern: '^_',
+};
+
+export default defineConfig([
+
+  // Set ESLint and Stylistic for Java- and TypeScript
   {
-    files: ['**/*.ts'],
-
+    files: ['**/*.js', '**/*.ts'],
     extends: [
-      JSeslint.configs.recommended,
-      TSeslint.configs.recommended,
-      TSeslint.configs.stylistic,
-      angular.configs.tsRecommended,
+      ESLintJS.configs.recommended,
+      stylistic.configs.customize({ severity: 'warn' }),
     ],
 
+    rules: { // Stylistic Rules: https://eslint.style/rules
+
+      '@stylistic/max-len': ['warn', {
+        code: 250,
+        ignoreUrls: true, ignoreStrings: true, ignoreTemplateLiterals: true,
+      }],
+
+      '@stylistic/linebreak-style': ['off'],
+      '@stylistic/semi': ['warn', 'always'],
+      '@stylistic/comma-dangle': ['warn', 'always-multiline'],
+      '@stylistic/quotes': ['warn', 'single', { avoidEscape: true }],
+
+      '@stylistic/array-element-newline': ['warn', 'consistent'],
+      '@stylistic/array-bracket-newline': ['warn', 'consistent'],
+    },
+  },
+
+  {
+    files: ['**/*.js'],
+    rules: { 'no-unused-vars': ['warn', unusedVarIgnorePattern] },
+  },
+
+  {
+    files: ['**/*.ts'],
+    extends: [
+      angular.configs.tsRecommended,
+      ESLintTS.configs.recommended,
+      ESLintTS.configs.stylistic,
+    ],
     processor: angular.processInlineTemplates,
 
     rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: '',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: '',
-          style: 'kebab-case',
-        },
-      ],
+      '@angular-eslint/directive-selector': ['error', { type: 'attribute', prefix: '', style: 'camelCase' }],
+      '@angular-eslint/component-selector': ['error', { type: 'element', prefix: '', style: 'kebab-case' }],
+
+      '@typescript-eslint/no-unused-vars': ['warn', unusedVarIgnorePattern],
     },
   },
 
@@ -41,8 +66,8 @@ export default defineConfig(
     files: ['**/*.html'],
     extends: [
       angular.configs.templateRecommended,
-      angular.configs.templateAccessibility
+      angular.configs.templateAccessibility,
     ],
-    rules: {},
   },
-);
+
+]);
