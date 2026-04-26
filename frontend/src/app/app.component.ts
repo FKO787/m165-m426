98f37c81996b6c8ad86d6da53830011c9f6377d6
FKO787 <flavio.koeppel@stud.bfstg.ch>
2026-04-26 11:18:15 +0200

    feat: adding login page, adding logout button to unimplemented page and created guard for correct redirecting when logged in/out

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'root',
-    imports: [RouterOutlet],
-  templateUrl: './app.html',
+  selector: "root",
+  imports: [RouterOutlet],
+  templateUrl: "./app.html",
 })
-export class App implements OnInit {
-    private auth = inject(AuthService);
-
-    ngOnInit() {
-        this.auth.restoreSession();
-    }
-}
+export class App {}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RegisterComponent } from '../components//loginPages/register/register.c
 import { NotImplementedComponent } from '../components/errorPages/notImplemented/notImplemented.component';
 import { authGuard } from '../services/auth.guard';
 import { redirectGuard } from '../services/rederect.guard';
+import { LoginComponent } from '../components/loginPages/login/login.component';
+import { publicGuard } from '../services/public.guard';
 
 export const routes: Routes = [
-    { path: 'register', title: 'Register', component: RegisterComponent },
-    { path: 'login', title: 'Login', component: NotImplementedComponent }, // TODO: LoginComponent
+    { path: 'register', title: 'Register', component: RegisterComponent, canActivate: [publicGuard] },
+    { path: 'login', title: 'Login', component: LoginComponent, canActivate: [publicGuard] },
 
     { path: '', title: 'Home', component: NotImplementedComponent, canActivate: [authGuard] }, // TODO: MainpageComponent
 
