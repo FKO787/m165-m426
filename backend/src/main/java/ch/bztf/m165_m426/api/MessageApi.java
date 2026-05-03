58ab6ee538d34dd0a198056b5e0f66b432bee716
ShadowOfThePit <shadowof.thepit@gmail.com>
2026-05-03 21:15:08 +0200

    feat: add rough messageApi

---
+++ backend/src/main/java/ch/bztf/m165_m426/api/MessageApi.java
@@ -0,0 +1,51 @@
+package ch.bztf.m165_m426.api;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.web.bind.annotation.*;
+
+import ch.bztf.m165_m426.entities.GlobalMessage;
+import ch.bztf.m165_m426.repositories.GlobalMessageRepository;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class MessageApi {
+
+    private final GlobalMessageRepository messageRepo;
+
+    public MessageApi(GlobalMessageRepository messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+    @GetMapping("/messages")
+    public List<GlobalMessage> getAllMessages() {
+        return messageRepo.findAll();
+    }
+
+    @GetMapping("/messages/{id}")
+    public GlobalMessage getMessage(@PathVariable Long id) {
+        return messageRepo.findById(id).orElseThrow();
+    }
+
+    @PostMapping("/messages")
+    public GlobalMessage sendMessage(
+            @RequestBody GlobalMessage message) {
+        return messageRepo.save(message);
+    }
+
+    @PostMapping("/messages/{id}/reply")
+    public GlobalMessage replyToMessage(@PathVariable Long id, @RequestBody GlobalMessage reply) {
+        messageRepo.findById(id).orElseThrow();
+        return messageRepo.save(reply);
+    }
+
+    @DeleteMapping("/messages/{id}")
+    public void deleteMessage(@PathVariable Long id) {
+        if (!messageRepo.existsById(id)) {
+            throw new NoSuchElementException("Message with id " + id + " not found");
+        }
+
+        messageRepo.deleteById(id);
+    }
+}
