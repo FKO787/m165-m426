c5b8cbb8aa80a0b07e59288fc9157305643aa85a
Soedel08 <148466137+Soedel08@users.noreply.github.com>
2026-05-11 08:50:19 +0000

    style: auto-cleanup [skip ci]

--- frontend/src/components/loginPages/register/register.component.ts
+++ frontend/src/components/loginPages/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { email, form, FormField, required, submit, validate } from '@angular/forms/signals';
-import {Router, RouterLink} from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ApiError, AuthService } from '../../../services/auth.service';
 import hashAlgorithm from '../../security/hashAlgorithm';
 import { LoginLayoutComponent } from '../loginLayout/loginLayout.component';
@@ -15,7 +15,7 @@ interface LoginData {
 @Component({
   selector: 'register',
   standalone: true,
-    imports: [LoginLayoutComponent, FormField, RouterLink],
+  imports: [LoginLayoutComponent, FormField, RouterLink],
   styleUrls: ['../loginPages.css'],
   templateUrl: './register.html',
 })
